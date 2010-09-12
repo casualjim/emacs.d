4f2fe286bae9c804307b78177f7826dce5e4253c
Ivan Porto Carrero <ivan@flanders.co.nz>
2010-09-12 21:56:14 +0200

    Adds cedet and ecb

--- init.el
+++ init.el
@@ -85,6 +85,43 @@
     (yas/load-directory "~/.emacs.d/my-ext/yasnippet-0.6.1c/snippets")
 (add-to-list 'load-path "~/.emacs.d/ensime/elisp")
 
+;; Load CEDET.
+;; See cedet/common/cedet.info for configuration details.
+(load-file "~/.emacs.d/my-ext/cedet-1.0pre6/common/cedet.el")
+
+
+;; Enable EDE (Project Management) features
+(global-ede-mode 1)
+
+;; Enable EDE for a pre-existing C++ project
+;; (ede-cpp-root-project "NAME" :file "~/myproject/Makefile")
+
+
+;; Enabling Semantic (code-parsing, smart completion) features
+;; Select one of the following:
+
+;; * This enables the database and idle reparse engines
+(semantic-load-enable-minimum-features)
+
+;; * This enables some tools useful for coding, such as summary mode
+;;   imenu support, and the semantic navigator
+(semantic-load-enable-code-helpers)
+
+;; * This enables even more coding tools such as intellisense mode
+;;   decoration mode, and stickyfunc mode (plus regular code helpers)
+;; (semantic-load-enable-gaudy-code-helpers)
+
+;; * This enables the use of Exuberent ctags if you have it installed.
+;;   If you use C++ templates or boost, you should NOT enable it.
+;; (semantic-load-enable-all-exuberent-ctags-support)
+
+;; Enable SRecode (Template management) minor-mode.
+;; (global-srecode-minor-mode 1)
+
+(add-to-list 'load-path
+             "~/.emacs.d/my-ext/ecb-2.40")
+(require 'ecb)
+
 (set-default-font "-outline-Consolas-normal-r-normal-normal-17-97-96-96-c-*-iso8859-1")
 
 (require 'vimpulse)
